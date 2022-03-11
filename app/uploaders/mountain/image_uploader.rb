7a39d97d8b20e50e2758906ba988d8fbc659e4b6
TKG1 <kouheideal@gmail.com>
2022-03-11 22:11:18 +0900

    modify:本番環境でcarrierwaveのデフォルト画像が表示されない問題を修正

--- app/uploaders/mountain/image_uploader.rb
+++ app/uploaders/mountain/image_uploader.rb
@@ -22,7 +22,7 @@ class Mountain::ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
   def default_url
-    'default_mountain'
+    'default_mountain.png'
   end
 
   # Process files as they are uploaded:
